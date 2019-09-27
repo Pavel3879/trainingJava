903b62ba4b68d61af6f4a62347b279f3f72178b0
Pavel3879 <vytyagov@mail.ru>
2019-09-27 12:35:58 +0300

    Практика №6

---
+++ Practical/src/lesson6/animals/Cat.java
@@ -0,0 +1,14 @@
+package lesson6.animals;
+
+public class Cat extends Animal {
+
+    public Cat() {
+        super(200, 0, 2);
+    }
+
+    @Override
+    public String toString() {
+        return "Кот";
+    }
+
+}
